a937c12fea8d60cb10112b3eabc6186fab9cc5f9
Adam Tillis <tillisadam@gmail.com>
2022-10-12 23:14:58 -0400

    feat: moved traverse to a member of Location

--- src/location.rs
+++ src/location.rs
@@ -11,13 +11,39 @@ impl <'a> Location<'a> {
         Location { name, latitude, longitude }
     }
 
-    pub fn distance_to(&self, other: &Location) -> f64 {
+    pub fn distance_to(&self, other: &Location, is_logged: bool) -> f64 {
         let delta_latitude = (self.latitude - other.latitude).to_radians();
         let delta_longitude = (self.longitude - other.longitude).to_radians();
         let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(), 2) +
             self.latitude.to_radians().cos() * other.latitude.to_radians().cos() *
                 f64::powi((delta_longitude / 2.0).sin(), 2);
 
-        return EARTH_RADIUS_IN_KILOMETERS * 2.0 * inner_central_angle.sqrt().asin();
+        let result = EARTH_RADIUS_IN_KILOMETERS * 2.0 * inner_central_angle.sqrt().asin();
+
+        if is_logged {
+            println!("Distance from {} to {} => {:.1}km", self.name, other.name, result);
+        }
+
+        return result;
+    }
+
+    pub fn traverse(route: &[&Location], is_logged: bool) -> f64 {
+        let mut total_distance = 0.0;
+        let mut previous: Option<&Location> = None;
+
+        for waypoint in route {
+            total_distance += match previous {
+                None => 0.0,
+                Some(location) => location.distance_to(waypoint, is_logged)
+            };
+
+            previous = Some(waypoint);
+        }
+
+        if is_logged {
+            println!("Total distance traveled => {:.1}km", total_distance);
+        }
+
+        return total_distance;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -29,27 +29,6 @@ fn old_main() {
     }
 }
 
-fn traverse(route: &[&Location]) -> f64 {
-    let mut total_distance = 0.0;
-    let mut previous: Option<&Location> = None;
-
-    for waypoint in route {
-        total_distance += match previous {
-            None => 0.0,
-            Some(location) => {
-                let result = location.distance_to(waypoint);
-                println!("Distance from {} to {} => {:.1}", location.name, waypoint.name, result);
-                result
-            }
-        };
-
-        previous = Some(waypoint);
-    }
-
-    println!("Total distance traveled => {:.1}", total_distance);
-    return total_distance;
-}
-
 fn main() {
     let bff = Location::new("BFF", 41.89420, -103.48200);
     let bryto = Location::new("BRYTO", 41.74170, -85.31320);
@@ -57,7 +36,7 @@ fn main() {
     let dbq = Location::new("DBQ", 42.40150, -90.70910);
     let fod = Location::new("FOD", 42.61110, -94.29480);
     let gij = Location::new("GIJ", 41.76860, -86.31850);
-    let kcle = Location::new("KCLE", 61.4075, -81.851111);
+    let kcle = Location::new("KCLE", 41.4075, -81.851111);
     let kslc = Location::new("KSLC", 40.7861, -111.9822);
     let leyir = Location::new("LEYIR", 41.51030, -83.88080);
     let modem = Location::new("MODEM", 41.72800, -84.89730);
@@ -73,7 +52,7 @@ fn main() {
     let wegem = Location::new("WEGEM", 41.44560, -109.99000);
     let zoser = Location::new("ZOSER", 41.72390, -84.78130);
 
-    traverse(&[
+    Location::traverse(&[
         &kcle,
         &leyir,
         &pions,
@@ -95,5 +74,5 @@ fn main() {
         &pudvy,
         &wegem,
         &kslc
-    ]);
+    ], true);
 }
